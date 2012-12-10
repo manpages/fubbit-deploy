24ebf93848911d43f1af295afbcedd80eec9ae3a
Jonns Mostovoys <amarr.industrial@gmail.com>
2012-12-10 16:29:29 +0200

    checks for existing directory now

--- get-fubbit.sh
+++ get-fubbit.sh
@@ -2,10 +2,12 @@ if [ ! -d "deps" ]; then
   mkdir "deps"
 fi
 
-cd deps ;\
-git clone https://github.com/manpages/fubbit.git ;\
-cd fubbit ;\
-make ;\
-cd .. ;\
-mv -ut . fubbit/deps/amqp_client ;\
-mv -ut . fubbit/deps/rabbit_common
+if [ ! -d "deps/amqp_client" ]; then
+  cd deps ;\
+  git clone https://github.com/manpages/fubbit.git ;\
+  cd fubbit ;\
+  make ;\
+  cd .. ;\
+  mv -ut . fubbit/deps/amqp_client ;\
+  mv -ut . fubbit/deps/rabbit_common
+fi
